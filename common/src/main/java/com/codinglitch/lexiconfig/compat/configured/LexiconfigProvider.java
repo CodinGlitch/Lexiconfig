97af8562e6c0398b71e8ff21ee3c65c26fcf8cf7
CodinGlitch <codinglitchdev@gmail.com>
2026-06-03 15:37:17 -0700

    [Common] Detached api from minecraft to target java 8

--- common/src/main/java/com/codinglitch/lexiconfig/compat/configured/LexiconfigProvider.java
+++ common/src/main/java/com/codinglitch/lexiconfig/compat/configured/LexiconfigProvider.java
@@ -1,6 +1,7 @@
 package com.codinglitch.lexiconfig.compat.configured;
 
 import com.codinglitch.lexiconfig.Lexiconfig;
+import com.codinglitch.lexiconfig.LexiconfigApi;
 import com.codinglitch.lexiconfig.Library;
 import com.codinglitch.lexiconfig.classes.LexiconData;
 import com.mrcrayfish.configured.api.IModConfig;
@@ -14,18 +15,18 @@ import java.util.Set;
 public class LexiconfigProvider implements IModConfigProvider {
     @Override
     public Set<IModConfig> getConfigurationsForMod(ModContext modContext) {
-        Lexiconfig.debug("Attempting to find configured configs for mod {}", modContext.modId());
+        LexiconfigApi.INSTANCE.debug("Attempting to find configured configs for mod {}", modContext.modId());
 
         Optional<Library> optionalLibrary = Lexiconfig.findLibrary(modContext.modId());
         if (optionalLibrary.isEmpty()) return Set.of();
 
-        Lexiconfig.info("Providing configured configs for valid mod {}", modContext.modId());
+        LexiconfigApi.INSTANCE.info("Providing configured configs for valid mod {}", modContext.modId());
 
         Library library = optionalLibrary.get();
         Set<IModConfig> configs = new HashSet<>();
         for (LexiconData lexicon : library.LEXICONS) {
             configs.add(new LexiconfiguredFile(library, lexicon));
-            Lexiconfig.info("Loaded lexiconfigured library", lexicon.getName());
+            LexiconfigApi.INSTANCE.info("Loaded lexiconfigured library", lexicon.getName());
         }
 
         return configs;
